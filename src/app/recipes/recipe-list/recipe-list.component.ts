bf43675bbf89deff262e876392853c9603893aa9
Justin Walker <walkerjt93@gmail.com>
2020-02-10 15:27:05 -0500

    actually was able to figure out how to seperate out the recipe-item component, and then pass it down from the recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from '../recipe.model'
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Chicken Pot Pie', 'A delicious southern classic', 'https://www.onceuponachef.com/images/2017/10/Chicken-Pot-Pie-2.jpg'),
-    new Recipe('Mashed Potatoes', 'Who doesn\'t love them?', 'https://scm-assets.constant.co/scm/unilever/e9dc924f238fa6cc29465942875fe8f0/6d5b546c-c322-4025-98ec-1bb1556fa7ad.jpg'),
+    new Recipe(
+      'Chicken Pot Pie',
+      'A delicious southern classic',
+      'https://www.onceuponachef.com/images/2017/10/Chicken-Pot-Pie-2.jpg'
+    ),
+    new Recipe(
+      'Mashed Potatoes',
+      "Who doesn't love them?",
+      'https://scm-assets.constant.co/scm/unilever/e9dc924f238fa6cc29465942875fe8f0/6d5b546c-c322-4025-98ec-1bb1556fa7ad.jpg'
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
