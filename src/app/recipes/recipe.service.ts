30b5922e62b9ce4980d1856781bb279b4ac21885
Justin Walker <walkerjt93@gmail.com>
2020-02-24 14:34:54 -0500

    submitting the Recipe Edit form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,9 +3,12 @@ import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
   private recipes: Recipe[] = [
     new Recipe(
       'Chicken Pot Pie',
@@ -30,6 +33,16 @@ export class RecipeService {
   ];
 
   constructor(private slService: ShoppingListService) {}
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipe(index: number) {
     return this.recipes[index];
