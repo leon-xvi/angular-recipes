a339bc8817583f8a746a59f5e4b350e7ea944e88
justin <walkerjt93@gmail.com>
2020-02-26 19:24:19 -0500

    added a resolver for the recipes paths

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,28 +9,7 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Chicken Pot Pie',
-      'A delicious southern classic',
-      'https://www.onceuponachef.com/images/2017/10/Chicken-Pot-Pie-2.jpg',
-      [
-        new Ingredient('chicken', 1),
-        new Ingredient('pot', 1),
-        new Ingredient('pie', 1)
-      ]
-    ),
-    new Recipe(
-      'Mashed Potatoes',
-      "Who doesn't love them?",
-      'https://scm-assets.constant.co/scm/unilever/e9dc924f238fa6cc29465942875fe8f0/6d5b546c-c322-4025-98ec-1bb1556fa7ad.jpg',
-      [
-        new Ingredient('potatoes', 5),
-        new Ingredient('butter', 2),
-        new Ingredient('garlic', 1)
-      ]
-    )
-  ];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
