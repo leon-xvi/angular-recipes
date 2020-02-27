5571079563531a0ccb1a286ebb20ff6dd0895754
Justin Walker <walkerjt93@gmail.com>
2020-02-27 15:18:28 -0500

    - generated auth component with the cli - creating a basic bootstrap form for auth - adding a route for the path /auth - added a link to the auth page in the header menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
+import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'recipes' },
@@ -27,6 +28,7 @@ const appRoutes: Routes = [
       }
     ]
   },
+  { path: 'auth', component: AuthComponent },
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
