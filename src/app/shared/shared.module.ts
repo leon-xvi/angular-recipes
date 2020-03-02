2e914f83eac85cc30cd5ab67851107203958a264
Justin Walker <walkerjt93@gmail.com>
2020-03-02 13:45:20 -0500

    seperated the Auth feature into its own module as well

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.compo
 import { PlaceholderDirective } from './placeholder/placeholder.directive';
 import { DropdownDirective } from './dropdown.directive';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
